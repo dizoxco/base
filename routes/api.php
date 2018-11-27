a84014becd878d3f2f56e1b922ee830ecb44e20f
omidshj <omid.shj@gmail.com>
2018-11-27 20:22:40 +0330

    route map

--- routes/api.php
+++ routes/api.php
@@ -1,16 +1,28 @@
 <?php
 
+Route::prefix('auth')->group(function () {
+    Route::post('login', 'AuthController@login');
+    Route::post('register', 'AuthController@register');
+    Route::get('register/activate/{token}', 'AuthController@registerActive')->name('api.user.active');
+    Route::group(['middleware' => 'auth:api'], function () {
+        Route::get('logout', 'AuthController@logout');
+        Route::get('user', 'AuthController@user');
+    });
+});
+
 Route::name('users.')->prefix('users')->group(function () {
-    Route::get('/', 'UserController@index' )->name('index');
+    Route::get('/', 'UserController@index' )->name('index')->middleware(['auth:api', 'permission:manage posts', 'ownership']);
     Route::post('/', 'UserController@store' )->name('store');
     Route::prefix('{user}')->group(function () {
         Route::get('/', 'UserController@show' )->name('show');
         Route::put('/', 'UserController@update' )->name('update');
         Route::delete('/', 'UserController@delete' )->name('delete');
-        Route::post('comments', 'UserController@comments')->name('comments');
-        Route::post('roles', 'UserController@roles')->name('roles');
-        Route::post('permissions', 'UserController@permissions')->name('permissions');
-        Route::post('posts', 'UserController@posts')->name('posts');
+        Route::get('comments', 'UserController@comments')->name('comments');
+        Route::get('roles', 'UserController@roles')->name('roles');
+        Route::post('roles', 'UserController@addRole')->name('roles.add');
+        Route::put('roles', 'UserController@syncRoles')->name('roles.sync');
+        Route::get('permissions', 'UserController@permissions')->name('permissions');
+        Route::get('posts', 'UserController@posts')->name('posts');
     });
 });
 
