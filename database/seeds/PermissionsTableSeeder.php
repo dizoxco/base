6d5549b09d9bf9b7e53785a80abbc0af9be8f907
Mohammad Akbari <akbarjimi@yahoo.com>
2018-12-22 13:55:17 +0330

    Create invidual seeder class for use in test suites 	new file:   database/seeds/CommentsTableSeeder.php 	new file:   database/seeds/PermissionsTableSeeder.php 	new file:   database/seeds/PostsTableSeeder.php 	new file:   database/seeds/RolesTableSeeder.php 	new file:   database/seeds/UsersTableSeeder.php

---
+++ database/seeds/PermissionsTableSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Permission;
+
+class PermissionsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $permissions    =   [
+            //  users permissions
+            ['name' =>  'manage_users', 'guard_name'    =>  'api'],
+
+            //  posts permissions
+            ['name' =>  'manage_posts', 'guard_name'    =>  'api'],
+
+            //  tickets permissions
+            ['name' =>  'manage_tickets', 'guard_name'  =>  'api'],
+        ];
+        foreach ($permissions as $permission) {
+            Permission::create($permission);
+        }
+    }
+}
---
+++ database/seeds/PostsTableSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+use App\Models\Post;
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class PostsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $numbers        =   (int) $this->command->ask('How many articles can be created per user?', 2);
+        $this->users->each(
+            function (User $user) use ($numbers) {
+                for ($i = 1; $i <= $numbers; $i++) {
+                    $user->posts()->create(factory(Post::class)->make()->toArray());
+                }
+            }
+        );
+        $this->command->line("Seeded {$numbers} posts for each users");
+    }
+}
