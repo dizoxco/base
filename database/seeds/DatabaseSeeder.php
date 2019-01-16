9297a724867eb44a5af44afc11754c04b76c2f31
Mohammad Akbari <mohammad.akbari@outlook.com>
2019-01-16 11:28:21 +0330

    Fixed bug #9 (User does not have the right permissions)
    Add custom seeder for test searchs and searchpanels

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -5,6 +5,8 @@ use App\Models\Post;
 use App\Models\User;
 use App\Models\Comment;
 use App\Models\Taxonomy;
+use App\Models\Variation;
+use App\Models\SearchPanel;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Collection;
 use Spatie\Permission\Models\Role;
@@ -13,7 +15,7 @@ use Spatie\Permission\Models\Permission;
 class DatabaseSeeder extends Seeder
 {
     protected $seeds = [
-        'migrate',
+//        'migrate',
         'permissions',
         'roles',
         'passport',
@@ -21,10 +23,23 @@ class DatabaseSeeder extends Seeder
         'users',
         'posts',
         'comments',
+//        'productPages'
     ];
 
     /** @var Collection $users */
     private $users;
+
+    /** @var Collection $brands */
+    private $brands;
+
+    /** @var Collection $colors */
+    private $colors;
+
+    /** @var Collection $types */
+    private $types;
+
+    /** @var Tag $cloth */
+    private $cloth;
 
     public function run()
     {
@@ -44,16 +59,19 @@ class DatabaseSeeder extends Seeder
     {
         $permissions = [
             //  users permissions
-            ['name' =>  'manage_users', 'guard_name'    =>  'api'],
+            ['name' =>  'manage_users',],
 
             //  posts permissions
-            ['name' =>  'manage_posts', 'guard_name'    =>  'api'],
+            ['name' =>  'manage_posts',],
 
             //  tickets permissions
-            ['name' =>  'manage_tickets', 'guard_name'  =>  'api'],
+            ['name' =>  'manage_tickets',],
 
             //  search panels permissions
-            ['name' =>  'manage_search_panels', 'guard_name'  =>  'api'],
+            ['name' =>  'manage_search_panels',],
+
+            //  businesses permissions
+            ['name' =>  'manage_businesses',],
         ];
         foreach ($permissions as $permission) {
             Permission::create($permission);
@@ -62,8 +80,8 @@ class DatabaseSeeder extends Seeder
 
     public function roles()
     {
-        Role::create(['name' => 'admin', 'guard_name' => 'api'])->givePermissionTo(Permission::all());
-        Role::create(['name' => 'user', 'guard_name' => 'api']);
+        Role::create(['name' => 'admin'])->givePermissionTo(Permission::all());
+        Role::create(['name' => 'user']);
     }
 
     public function passport()
@@ -73,22 +91,28 @@ class DatabaseSeeder extends Seeder
 
     public function tags()
     {
-        Taxonomy::create(['group_name' => 'brand', 'label' => 'برند']);
-        Taxonomy::create(['group_name' => 'color', 'label' => 'رنگ']);
-        Taxonomy::create(['group_name' => 'mobile', 'label' => 'موبایل']);
-        Taxonomy::create(['group_name' => 'shirt', 'label' => 'پیراهن']);
+        $this->brands = Taxonomy::create(['group_name' => 'brands', 'slug' => 'brands', 'label' => 'برندها'])
+            ->tags()->createMany([
+                ['label' => 'اپل', 'slug' => 'apple'],
+                ['label' => 'نوکیا', 'slug' => 'nokia'],
+                ['label' => 'کلمبیا', 'slug' => 'colombia'],
+            ]);
 
-        Tag::create(['group_id' => 1, 'label' => 'سونی', 'slug' => 'sony']);
-        Tag::create(['group_id' => 1, 'label' => 'نوکیا', 'slug' => 'nokia']);
-        Tag::create(['group_id' => 1, 'label' => 'هواوی', 'slug' => 'huawei']);
+        $this->colors = Taxonomy::create(['group_name' => 'colors', 'slug' => 'colors', 'label' => 'رنگ ها'])
+            ->tags()->createMany([
+                ['label' => 'قرمز', 'slug' => 'red'],
+                ['label' => 'سبز', 'slug' => 'green'],
+                ['label' => 'ابی', 'slug' => 'blue'],
+            ]);
 
-        Tag::create(['group_id' => 2, 'label' => 'قرمز', 'slug' => 'red']);
-        Tag::create(['group_id' => 2, 'label' => 'ابی', 'slug' => 'blue']);
-        Tag::create(['group_id' => 2, 'label' => 'سبز', 'slug' => 'green']);
-        Tag::create(['group_id' => 2, 'label' => 'زرد', 'slug' => 'yellow']);
+        $this->types = Taxonomy::create(['group_name' => 'types', 'slug' => 'types', 'label' => 'دستگاه ها'])
+            ->tags()->createMany([
+                ['label' => 'موبایل', 'slug' => 'mobile'],
+                ['label' => 'لپ تاپ', 'slug' => 'laptop'],
+            ]);
 
-        Tag::create(['group_id' => 3, 'label' => 'موبایل', 'slug' => 'mobile']);
-        Tag::create(['group_id' => 4, 'label' => 'پیراهن', 'slug' => 'shirt']);
+        $this->cloth = Taxonomy::create(['group_name' => 'clothes', 'slug' => 'clothes', 'label' => 'البسه'])
+            ->tags()->create(['label' => 'پیراهن', 'slug' => 'shirt']);
     }
 
     public function users()
@@ -129,5 +153,143 @@ class DatabaseSeeder extends Seeder
             }
         });
         $this->command->line("Seeded {$numbers} comments for each posts");
+    }
+
+    public function productPages()
+    {
+        $faker = Faker\Factory::create('fa_IR');
+
+        $titles = ['آیفون 32', 'ایفون 64', 'ایفون 128', 'مک بوک 13', 'مک بوک 15', 'مک بوک ایر'];
+        $slugs = ['iphone32', 'iphone64', 'iphone128', 'macbook13', 'macbook15', 'macbookair'];
+
+        foreach ($titles as $index => $title) {
+            /** @var User $user */
+            $user = $this->users->random();
+            /** @var Variation $product_page */
+            $product_page = $user->productsPages()->create([
+                'title' =>  $title,
+                'slug'  =>  $slugs[$index],
+                'abstract' => $faker->sentence,
+                'body' => $faker->sentences(3, true),
+                'attributes' => json_encode([
+                        'CPU' => $faker->macProcessor,
+                        'RAM' => mt_rand(3, 16).' GB',
+                        'LCD' => mt_rand(5, 15).' Inch',
+                    ]),
+            ]);
+
+            $selected_colors = $this->colors->random(mt_rand(1, 3))->pluck('slug', 'id')->toArray();
+            $selected_warranties = array_random(['Nic', 'PG', 'without'], mt_rand(1, 3));
+            $variations = [];
+            foreach ($selected_colors as $color) {
+                foreach ($selected_warranties as $warranty) {
+                    if ($faker->boolean(35)) {
+                        continue;
+                    }
+                    $product = $product_page->products()->create([
+                        'options' => json_encode([
+                            'color' =>  $color,
+                            'warranty' =>  $warranty,
+                        ]),
+                        'price' => mt_rand(1000000, 10000000),
+                        'quantity' => mt_rand(0, 5),
+                    ]);
+                    $variations[] = [
+                        'pid' => $product->id,
+                        'color' =>  $color,
+                        'warranty' =>  $warranty,
+                    ];
+                }
+            }
+            $product_page->update(['variations' => json_encode($variations)]);
+            $product_page->tags()->syncWithoutDetaching(array_keys($selected_colors));
+            $product_page->tags()->syncWithoutDetaching($this->brands->firstWhere('slug', 'apple')->id);
+            if ($index <= 2) {
+                $product_page->tags()->syncWithoutDetaching($this->types->firstWhere('slug', 'mobile')->id);
+            } else {
+                $product_page->tags()->syncWithoutDetaching($this->types->firstWhere('slug', 'laptop')->id);
+            }
+        }
+
+        SearchPanel::create([
+            'title' => 'محصولات اپل',
+            'slug' => 'apple',
+            'description' => $faker->sentences(3, true),
+            'model' => Variation::class,
+            'options' => json_encode([
+                'color' => [
+                    'label' =>  'Rang',
+                    'query' =>  'tag',
+                    'tag'  =>  $this->colors->map(function ($tag) {
+                        return ['id' => $tag->id, 'label' => $tag->label];
+                    })->toArray(),
+                ],
+            ]),
+            'filters' => json_encode([
+                'brand' => [
+                    'label' =>  'Rang',
+                    'query' =>  'tag',
+                    'tag'  =>  $this->brands->map(function ($tag) {
+                        if ($tag->slug === 'apple') {
+                            return ['id' => $tag->id, 'label' => $tag->label];
+                        }
+                    })->filter()->toArray(),
+                ],
+            ]),
+        ]);
+
+        SearchPanel::create([
+            'title' => 'محصولات اپل - آیفون',
+            'slug' => 'iphone',
+            'description' => $faker->sentences(3, true),
+            'model' => Variation::class,
+            'options' => json_encode([
+                'color' => [
+                    'label' =>  'Rang',
+                    'query' =>  'tag',
+                    'tag'  =>  $this->colors->map(function ($tag) {
+                        return ['id' => $tag->id, 'label' => $tag->label];
+                    })->toArray(),
+                ],
+            ]),
+            'filters' => json_encode([
+                'brand' => [
+                    'label' =>  'Rang',
+                    'query' =>  'tag',
+                    'tag'  =>  $this->types->map(function ($tag) {
+                        if ($tag->slug == 'mobile') {
+                            return ['id' => $tag->id, 'label' => $tag->label];
+                        }
+                    })->filter()->toArray(),
+                ],
+            ]),
+        ]);
+
+        SearchPanel::create([
+            'title' => 'محصولات اپل - مکبوک',
+            'slug' => 'macbook',
+            'description' => $faker->sentences(3, true),
+            'model' => Variation::class,
+            'options' => json_encode([
+                'color' => [
+                    'label' =>  'Rang',
+                    'query' =>  'tag',
+                    'tag'  =>  $this->colors->map(function ($tag) {
+                        return ['id' => $tag->id, 'label' => $tag->label];
+                    })->toArray(),
+                ],
+            ]),
+            'filters' => json_encode([
+                'brand' => [
+                    'label' =>  'Rang',
+                    'query' =>  'tag',
+                    'tag'  =>  $this->types->map(function ($tag) {
+                        if ($tag->slug == 'laptop') {
+                            return ['id' => $tag->id, 'label' => $tag->label];
+                        }
+                    })->filter()->toArray(),
+                ],
+            ]),
+        ]);
     }
 }
