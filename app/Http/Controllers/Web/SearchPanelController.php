7cb01f0c7ef325d6915df0f2b0bfebb3f8111f2f
Mohammad Akbari <38979273+akbarjimi@users.noreply.github.com>
2019-01-27 18:30:58 +0330

    Apply fixes from StyleCI (#20)
    [ci skip] [skip ci]

--- app/Http/Controllers/Web/SearchPanelController.php
+++ app/Http/Controllers/Web/SearchPanelController.php
@@ -2,15 +2,16 @@
 
 namespace App\Http\Controllers\Web;
 
-use App\Http\Controllers\Controller;
 use App\Models\SearchPanel;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 
 class SearchPanelController extends Controller
 {
     public function search(Request $request, SearchPanel $searchPanel)
     {
         $products = $searchPanel->result($request);
+
         return view('searchpanels.index', compact('products'));
     }
 }
