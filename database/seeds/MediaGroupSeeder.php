7c788c3cf949f0e4f1627d1d7292c1643de78502
Mohammad Akbari <38979273+akbarjimi@users.noreply.github.com>
2019-01-28 15:33:05 +0330

    Apply fixes from StyleCI (#23)
    [ci skip] [skip ci]

--- database/seeds/MediaGroupSeeder.php
+++ database/seeds/MediaGroupSeeder.php
@@ -10,14 +10,14 @@ class MediaGroupSeeder extends Seeder
     {
         $faker = Factory::create('fa_IR');
         $mgp = MediaGroup::create(['name' => 'posts', 'collection_name' => 'posts', 'description' => '']);
-        foreach (range(1,20) as $key) {
+        foreach (range(1, 20) as $key) {
             $mgp->addMediaFromUrl(
                 base_path('resources/seed/blog-images/'.$key.'.jpg')
             )->toMediaCollection($mgp->name);
         }
 
         // factory(MediaGroup::class, 1)->create(['name' => 'posts', 'collection_name' => 'posts'])->each(function (MediaGroup $mediaGroup) use ($faker) {
-            
+
         // });
     }
 }
