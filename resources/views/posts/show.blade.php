a9e63390c94eca6d3adff97728472594f6155189
hananeh malekzadeh <hananeh.farkoush@gmail.com>
2019-01-28 17:44:58 +0330

    show.balde blog + footer layout

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -1,11 +1,20 @@
 @extends('layout')
 @section('content')
     <div class="flex flex-wrap">
-        <div class="w-4/5">
+        <div class="w-4/5 mb-10">
+            {{-- @php print_r($post) @endphp --}}
             <img src="{{$post->banner[0]->getUrl('banner')}}" alt="">
-            <h1>{{$post->title}}</h1>
+            <div class="py-2"><time datetime="YYYY.MM.DD">{{$post->published_at}}</time></div>
+            <h1 class="mb-4">{{$post->title}}</h1>
             <div>
                 {{$post->body}}
+            </div>
+            <div class="related-post" column="2">
+                <div class="">
+                    {{-- @foreach ($posts as $post)
+                        @component('components.post.card-horizontal',['post'=>$post])@endcomponent
+                    @endforeach --}}
+                </div>
             </div>
         </div>
         <div class="w-1/5">
