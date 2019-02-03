b3efcfecb3d3895ddeee3f5c0c34eaa78bd62192
Mohammad Akbari <mohammad.akbari@outlook.com>
2019-02-03 19:03:32 +0330

    Add iran cities, counties and provinces references to app

---
+++ app/Models/City.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class City extends Model
+{
+    public function county()
+    {
+        return $this->belongsTo(County::class, 'county_id','id');
+    }
+
+    public function province()
+    {
+        return $this->belongsTo(Province::class, 'province_id','id');
+    }
+}
