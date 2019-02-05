8f0b7cf345a3350b19709b039e5c2d412bbb2908
hananeh malekzadeh <hananeh.farkoush@gmail.com>
2019-02-05 12:04:54 +0330

    grid products filter

--- resources/views/searchpanels/inputs/checkbox.blade.php
+++ resources/views/searchpanels/inputs/checkbox.blade.php
@@ -1,11 +1,11 @@
-<input
-        type="checkbox"
-        name="{{ $name }}[]"
-        value="{{ $index }}"
-        @if(request()->get($name) !== null)
-            @if(in_array($index, request()->get($name)))
-                checked
-            @endif
-        @endisset
->
-<hr>
+<input 
+class="body-1" 
+type="checkbox" 
+name="{{ $name }}[]" 
+value="{{ $index }}" 
+@if(request()->get($name) !== null)
+@if(in_array($index, request()->get($name)))
+    checked
+@endif
+@endisset
+> 
--- resources/views/searchpanels/products.blade.php
+++ resources/views/searchpanels/products.blade.php
@@ -1,39 +1,78 @@
 @extends('layout')
 @section('content')
     <div class="flex flex-wrap">
-        <div class="w-1/4">
-            <h4>فیلترها</h4>
+        <div class="w-1/4 pl-4">
             <form target="_self">
                 {{-- <p >{{ $options['order']['label'] }}</p> --}}
                 @forelse($options as $name => $option)
-                    <label >{{ $option['label'] }}</label>
-                    {{-- <hr> --}}
+                @if ($option['query'] !== 'order')
+                <div class="relative bg-white rounded-lg pt-2 pb-4 px-4 mb-4">
+                    <p class="body-2" >{{ $option['label'] }}</p>
                    @forelse(array_wrap($option[$option['query']]) as $index => $filter)
-                        @isset($filter['label'])
-                            <label >{{ $filter['label'] }}</label>
-                        @endisset
                         @switch($option['query'])
                             @case('tag')
-                                @include('searchpanels.inputs.checkbox', compact('name'))
+                                    @include('searchpanels.inputs.checkbox', compact('name'))
+                                    @isset($filter['label'])
+                                        <label >{{ $filter['label'] }}</label>
+                                    @endisset
+                                    <br>
                                 @break
                             @case('like')
                                 @include('searchpanels.inputs.textbox', compact('name', 'option'))
                                 @break
-                            @case('order')
+                            {{-- @case('order')
                                 @include('searchpanels.inputs.link', compact('name','index', 'filter'))
-                                @break
+                                @break --}}
                         @endswitch
                     @empty
                         <p>هیچ فیلتری تعریف نشده است</p>
                     @endforelse
+                </div>
+                @endisset
+
                 @empty
                     <p>انتخابی وجود ندارد.</p>
                 @endforelse
-                <button type="submit" class="-btn border-blue-dark border-2">search</button>
+                {{-- 
+                @foreach ($options as $name => $option)
+                    
+                    @if ($option['query'] == 'tag')
+                    <div class="relative bg-white rounded-lg py-2 px-4 my-4">
+                        <p class="body-2">{{$option['label']}}</p>
+                        @foreach ($option['tag'] as $index => $tag)
+                            <input 
+                                class="body-1" 
+                                type="checkbox" 
+                                name="{{ $name }}[]" 
+                                value="{{ $index }}" 
+                                @if(request()->get($name) !== null)
+                                @if(in_array($index, request()->get($name)))
+                                    checked
+                                @endif
+                                @endisset
+                            > {{$tag['label']}} </br>   
+                        @endforeach
+                    </div>
+                    @endif
+                    
+                    @if ($option['query'] == 'like')
+                        <p class="body-2">{{$option['label']}}</p>
+                        <input 
+                            class="shadow appearance-none border rounded w-full py-2 px-3 text-grey-darker leading-tight focus:outline-none focus:shadow-outline"
+                            type="text" 
+                            name="{{ $name }}"
+                            @if(request()->get($name) !== null)
+                            value="{{ request()->get($name) }}"
+                            @endisset
+                        >
+
+                    @endif
+                @endforeach --}}
+                <button type="submit" class="bg-blue hover:bg-blue-dark text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">search</button>
             </form>
         </div>
-        <div class="w-3/4">
-            <div class="relative bg-white rounded-lg p-4">
+        <div class="w-3/4 pr-4">
+            <div class="relative bg-white rounded-lg p-4 mb-2">
                 <span class="absoulute pin-r"><i class="material-icons">sort</i></span>
                 @foreach ($options as $option)
                     @if ($option['query'] == 'order')
