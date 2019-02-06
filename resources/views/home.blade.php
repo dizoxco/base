6b0710b9ab06d12df9a7d8630d2ec8a880d3d246
Mohammad Akbari <mohammad.akbari@outlook.com>
2019-02-06 18:37:51 +0330

    fix view

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,18 @@
 @extends('layout')
 @section('content')
+    <ul>
+        <li>
+            <a href="{{ route('web.authlogout') }}"
+               onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                Logout
+            </a>
+
+            <form id="logout-form" action="{{ route('web.authlogout') }}" method="POST" style="display: none;">
+                {{ csrf_field() }}
+            </form>
+        </li>
+    </ul>
     <div class="swiper simple -mx-4 overflow-hidden" column="3" >
         <div class="swiper-wrapper">
             @foreach ($posts as $post)
@@ -29,5 +42,5 @@
             </div>
         @endforeach
     </div>
-    
+
 @endsection
