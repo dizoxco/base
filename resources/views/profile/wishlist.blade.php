702b99fdfc3b4569b2f56f90cc15057ef0f5b8cb
Mohammad Akbari <mohammad.akbari@outlook.com>
2019-02-09 19:43:30 +0330

    move wishlist from profile to standalone address

--- resources/views/profile/wishlist.blade.php
+++ resources/views/profile/wishlist.blade.php
@@ -11,6 +11,20 @@
         <li>
             <a href="{{ route('products.show', $product->slug) }}"> مشاهده محصول </a>
         </li>
+        <li>
+            <a
+                    style="text-decoration: none"
+                    href="{{ route('wishlist.destroy', $product->slug) }}"
+                    onclick="event.preventDefault();
+                    document.getElementById('remove_product_from_wishlist').submit();"
+            >
+                حذف از علاقه مندی ها
+            </a>
+            <form id="remove_product_from_wishlist" action="{{ route('wishlist.destroy', $product->slug) }}" method="post" style="display: none;">
+                {{ csrf_field() }}
+                {{ method_field('delete') }}}
+            </form>
+        </li>
     @empty
         You have no product in your wish list yet!
     @endforelse
