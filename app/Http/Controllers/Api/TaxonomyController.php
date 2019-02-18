6ec4d35a74d5474dd95221229b7257e22783bf92
joker <masouddarvishi56@yahoo.com>
2019-02-18 15:37:58 +0330

    adding taxonomy respository , controller

---
+++ app/Http/Controllers/Api/TaxonomyController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Repositories\Facades\TaxonomyRepo;
+use App\Http\Controllers\Controller;
+
+class TaxonomyController extends Controller
+{
+    public function index()
+    {
+        return TaxonomyRepo::getAll(['with'=>'tags']);
+    }
+}
