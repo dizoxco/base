1d68ee4cdf7e78673f3a42102793557180ba6b63
Mohammad Akbari <mohammad.akbari@outlook.com>
2019-02-18 06:36:49 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -5,7 +5,6 @@ use Faker\Factory as Faker;
 $faker = Faker::create('fa_IR');
 
 $factory->define(App\Models\Product::class, function () use ($faker) {
-
     $options = array_random([
         'color' => [
             'label' => 'رنگ',
@@ -14,7 +13,7 @@ $factory->define(App\Models\Product::class, function () use ($faker) {
                 'red' => ['label' => 'قرمز', 'color' => '#e5e5e5'],
                 'pink' => ['label' => 'صورتی', 'color' => '#dddddd'],
                 'black' => ['label' => 'مشکی', 'color' => '#555555'],
-                'yellow' => ['label' => 'زرد', 'color' => '#aaaaaa']
+                'yellow' => ['label' => 'زرد', 'color' => '#aaaaaa'],
             ], rand(1, 4)),
         ],
         'size' => [
@@ -24,7 +23,7 @@ $factory->define(App\Models\Product::class, function () use ($faker) {
                 's' => ['label' => 's'],
                 'm' => ['label' => 'm'],
                 'l' => ['label' => 'l'],
-                'xl' => ['label' => 'xl']
+                'xl' => ['label' => 'xl'],
             ], rand(1, 4)),
         ],
         'capacity' => [
@@ -34,9 +33,9 @@ $factory->define(App\Models\Product::class, function () use ($faker) {
                 '8' => ['label' => '8 GB'],
                 '16' => ['label' => '16 GB'],
                 '32' => ['label' => '32 GB'],
-                '64' => ['label' => '64 GB']
-            ],rand(1, 4)),
-        ]
+                '64' => ['label' => '64 GB'],
+            ], rand(1, 4)),
+        ],
     ], rand(1, 3));
 
     return [
