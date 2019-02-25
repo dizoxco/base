d9bb32ec82f567ca359cda49b6d20a83fd18e69e
Your Name <dizox@yahoo.com>
2019-02-25 13:18:50 +0330

    business and profile sem final

--- resources/views/profile/businesses/layout.blade.php
+++ resources/views/profile/businesses/layout.blade.php
@@ -1,20 +1,21 @@
 @extends('layout', ['profile_mode' => true, 'title' => $title ?? 'عنوان بده'])
 @section('content')
     <div id="profile-layout">
-        <div class="profile-nav fixed h-full">
-            <br>
-            <br>
-            <br>
-            <ul class="mdc-list">
+        <div class="profile-nav fixed h-full" style="top:64px">
+            <div class="side__loged-user text-left -mr-16 bg-grey-darker rounded-l-full flex items-center justify-end ml-12 mt-4">
+                <a href="{{route('profile.businesses.products.index', $business->slug)}}" class="text-white bg-grey-dark rounded-full py-2 px-5 pl-12 -ml-6 text-sm">{{$business->brand}}</a>
+                <img class="w-24 h-24 rounded-full border-8 border-solid border-grey-darker " src="{{auth()->user()->getFirstMedia('avatar') ? auth()->user()->getFirstMedia('avatar')->getUrl() : '/images/avatar.jpg'}}" alt="">
+            </div>
+            <ul class="mdc-list profile-mdc-list">
                 <a href="{{ route('profile.businesses.products.index',$business->slug) }}" class="block">
                     <li class="mdc-list-item mdc-ripple" tabindex="0">
-                        <span class="mdc-list-item__graphic material-icons" aria-hidden="true">people</span>
+                        <span class="mdc-list-item__graphic material-icons" aria-hidden="true">shopping_cart</span>
                         <span class="mdc-list-item__text">محصولات</span>
                     </li>
                 </a>
                 <a href="{{ route('profile.businesses.orders.index',$business->slug) }}" class="block">
                     <li class="mdc-list-item mdc-ripple">
-                        <span class="mdc-list-item__graphic material-icons" aria-hidden="true">place</span>
+                        <span class="mdc-list-item__graphic material-icons" aria-hidden="true">attach_money</span>
                         <span class="mdc-list-item__text">سفارشات</span>
                     </li>
                 </a>
@@ -22,6 +23,18 @@
                     <li class="mdc-list-item mdc-ripple">
                         <span class="mdc-list-item__graphic material-icons" aria-hidden="true">chat</span>
                         <span class="mdc-list-item__text">چت ها</span>
+                    </li>
+                </a>
+                <a href="{{ route('profile.businesses.edit', $business->slug) }}" class="block">
+                    <li class="mdc-list-item mdc-ripple" tabindex="0">
+                        <span class="mdc-list-item__graphic material-icons" aria-hidden="true">edit</span>
+                        <span class="mdc-list-item__text">ویرایش کسب و کار</span>
+                    </li>
+                </a>
+                <a href="{{ route('logout') }}" class="block">
+                    <li class="mdc-list-item mdc-ripple" tabindex="0">
+                        <span class="mdc-list-item__graphic material-icons" aria-hidden="true">power_settings_new</span>
+                        <span class="mdc-list-item__text">خروج</span>
                     </li>
                 </a>
             </ul>
