9cd85ad26b60204d2efd69dfca16c0ccbde6c1e9
Mohammad Akbari <mohammad.akbari@outlook.com>
2019-02-25 13:21:15 +0330

    Check Business Edit

--- database/factories/BusinessFactory.php
+++ database/factories/BusinessFactory.php
@@ -10,24 +10,7 @@ $factory->define(Business::class, function () use ($faker) {
         'brand' => $faker->company,
         'slug' => str_slug("{$faker->company}_{$faker->numerify('###')}"),
         'city_id' => \faker('city')->first()->id,
-        'contact' => json_encode([
-            'tel' => [
-                ['label' => 'کارخانه', 'value' => $faker->phoneNumber],
-                ['label' => 'دفتر', 'value' => $faker->phoneNumber],
-            ],
-            'instagram' => [
-                ['label' => 'کارخانه', 'value' => $faker->url],
-                ['label' => 'دفتر', 'value' => $faker->url],
-            ],
-            'address' => [
-                ['label' => 'کارخانه', 'value' => $faker->address, 'extra' => [
-                    'label' => 'کد پستی', 'value' => rand(111111, 999999),
-                ]],
-                ['label' => 'دفتر', 'value' => $faker->address, 'extra' => [
-                    'label' => 'کد پستی', 'value' => rand(111111, 999999),
-                ]],
-            ],
-        ]),
+        'contact' => json_encode([]),
         'status' => 1,
         'created_at' => $created_at = $faker->dateTimeBetween('-5 years', 'now'),
         'updated_at' => $faker->dateTimeBetween($created_at, 'now'),
