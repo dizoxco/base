b97f149060bbc713bd0d65d465cbc30a468584f9
omidshj <omid.shj@gmail.com>
2019-02-28 11:19:26 +0330

    tinymcs fa ir bug

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <script src="//cdn.tinymce.com/4/tinymce.min.js"></script>
-    <script src="/js/fa_IR.js"></script>
+    {{-- <script src="/js/fa_IR.js"></script> --}}
     <title>Document</title>
     <link href="{{ asset('css/admin.css') }}" rel="stylesheet">
 </head>
