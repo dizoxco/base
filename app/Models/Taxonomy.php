40728cc8efd40d47ec75e06c91d6bb560b96856e
Mohammad Akbari <mohammad.akbari@outlook.com>
2019-03-03 15:43:13 +0330

    Make Tag sluggable

--- app/Models/Taxonomy.php
+++ app/Models/Taxonomy.php
@@ -3,9 +3,13 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Spatie\Sluggable\HasSlug;
+use Spatie\Sluggable\SlugOptions;
 
 class Taxonomy extends Model
 {
+    use HasSlug;
+
     /** @var string $table */
     protected $table = 'taxonomies';
 
@@ -17,5 +21,13 @@ class Taxonomy extends Model
     public function tags()
     {
         return $this->hasMany(Tag::class, 'taxonomy_id', 'id');
+    }
+
+    public function getSlugOptions(): SlugOptions
+    {
+        return SlugOptions::create()
+            ->usingLanguage('fa')
+            ->generateSlugsFrom('group_name')
+            ->saveSlugsTo('slug');
     }
 }
